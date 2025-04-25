9f13970b1c23b79bdb60274b7bb5eb7f35483a6b
hectorv <hectorfabiovasquez@outlook.com>
2025-04-25 08:07:20 -0500

    Created splash activity

---
+++ app/src/main/java/com/example/online_store/Activities/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.online_store.Activities
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.online_store.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
