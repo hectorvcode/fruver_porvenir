afccf193e0994d98029bec93bad606591dcb7650
hectorv <hectorfabiovasquez@outlook.com>
2025-04-25 19:48:32 -0500

    Added Home Activity

--- app/src/main/java/com/example/online_store/Activities/SplashActivity.kt
+++ app/src/main/java/com/example/online_store/Activities/SplashActivity.kt
@@ -1,14 +1,26 @@
 package com.example.online_store.Activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.online_store.R
 
 class SplashActivity : AppCompatActivity() {
 
+    private val SPLASH_TIME_OUT : Long = 3000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
+
     }
 }
