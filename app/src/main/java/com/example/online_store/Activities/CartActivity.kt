3d66a2babd38254174265f0c9929a73e7673626e
hectorv <hectorfabiovasquez@outlook.com>
2025-04-27 15:00:23 -0500

    Added CartActivity.kt and AndroidManifest modification

---
+++ app/src/main/java/com/example/online_store/Activities/CartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.online_store.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.online_store.R
+
+class CartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart)
+    }
+}
