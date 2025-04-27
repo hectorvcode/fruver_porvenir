6cd842086f7afdfcdd80971a40dc168becd5d56d
hectorv <hectorfabiovasquez@outlook.com>
2025-04-27 13:47:40 -0500

    Finished login activity

--- app/src/main/java/com/example/online_store/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/online_store/Activities/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.online_store.Activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.online_store.R
 
@@ -8,5 +10,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnComenzar : Button = findViewById(R.id.btn_comenzar)
+        btnComenzar.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/online_store/Activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.online_store.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.online_store.R
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
